e32f128ee2258d1e5b16610131e8e09791ccea78
dwincahya <dwincahya8@gmail.com>
2024-10-04 18:07:50 +0700

    Light Store

--- src/components/gamelist.jsx
+++ src/components/gamelist.jsx
@@ -15,26 +15,27 @@ const games = [
 
 const GameList = () => {
   return (
-    <div className="container mx-auto px-20 py-10"> {/* Add px-4 for padding on sides */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 md:gap-6"> {/* 5 columns on large screens */}
+    <div className="container mx-auto px-6 md:px-10 py-6 md:py-10"> {/* Reduced padding for a cleaner look */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-2 md:gap-4"> {/* Reduced gap for a tighter grid */}
         {games.map((game, index) => (
           <div
             key={index}
             className="relative cursor-pointer overflow-hidden rounded-lg shadow-lg group"
           >
             <div className="overflow-hidden">
-              <img
-                src={game.image}
-                alt={game.name}
-                className="w-full min-h-64 object-cover transition-transform duration-300 ease-in-out transform group-hover:scale-105 group-hover:opacity-60"
-              />
+                <img
+                    src={game.image}
+                    alt={game.name}
+                    className="w-full h-32 md:h-72 object-cover transition-transform duration-300 ease-in-out transform group-hover:scale-105 group-hover:opacity-60" 
+                />
             </div>
+
 
             <div className="absolute inset-0 border-4 border-transparent group-hover:border-blue-700 transition-all duration-300 ease-in-out"></div>
 
-            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-70 text-white p-2 md:p-4 opacity-0 translate-y-full group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-500 ease-in-out">
-              <h3 className="text-sm md:text-lg font-bold">{game.name}</h3>
-              <p className="text-xs md:text-sm">{game.publisher}</p>
+            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-70 text-white p-1 md:p-2 opacity-0 translate-y-full group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-500 ease-in-out">
+              <h3 className="text-xs md:text-sm font-bold">{game.name}</h3> 
+              <p className="text-xs">{game.publisher}</p> 
             </div>
           </div>
         ))}
