5f0b0daeaf2a48190e6386a6dfeb0df9a9434a70
dwincahya <dwincahya8@gmail.com>
2024-10-06 12:57:56 +0700

    Light Store

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'src': resolve(__dirname, 'src'), // menambahkan alias untuk folder src
-    },
-  },
-});
+})
+e
