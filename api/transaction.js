839d8d56ec548c96608bcc8c57963aaad8633260
dwincahya <dwincahya8@gmail.com>
2024-10-06 15:54:39 +0700

    Light Store

--- api/transaction.js
+++ api/transaction.js
@@ -1,27 +1,20 @@
+// /api/server.js
 import express from 'express';
 import cors from 'cors';
-import transactions from '../src/data/invoice.js';  
+import transactions from '../src/data/invoice.js'; 
 
 const app = express();
 app.use(cors());
 
-app.get('/api/transactions', (req, res) => {
-  res.json(transactions);
-});
-
-app.get('/api/transactions/:invoiceNumber', (req, res) => {
+app.get('/transactions/:invoiceNumber', (req, res) => {
   const { invoiceNumber } = req.params;
-
-  const result = transactions.filter(transaction =>
+  const result = transactions.filter(transaction => 
     transaction.invoiceNumber === invoiceNumber
   );
-
   if (result.length === 0) {
-    return res.status(404).json({ message: "Data tidak ditemukan" });
+    return res.status(404).json({ message: "Data not found" });
   }
-
   res.json(result);
 });
 
-// Serverless Function Handler
 export default app;
