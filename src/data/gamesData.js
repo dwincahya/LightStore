270931a0d002f7cc42f9d8fb9c4c850924d2f93a
dwincahya <dwincahya8@gmail.com>
2024-10-10 12:36:47 +0700

    Light Store

--- src/data/gamesData.js
+++ src/data/gamesData.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import img1 from '../assets/img/kagura.jpg';
 import img2 from '../assets/img/raiden.jpg';
 import img3 from '../assets/img/ff.jpg';
@@ -9,18 +8,6 @@ import img7 from '../assets/img/soldier11.jpg';
 import img8 from '../assets/img/wildrift.jpg';
 import img9 from '../assets/img/tof.jpg';
 import img10 from '../assets/img/robin.jpg';
-=======
-import img1 from '../assets/img/kagura.jpg'; 
-import img2 from '../assets/img/raiden.jpg'; 
-import img3 from '../assets/img/ff.jpg';
-import img4 from '../assets/img/hok.jpg'; 
-import img5 from '../assets/img/wuwa.png'; 
-import img6 from '../assets/img/pubgm.jpg'; 
-import img7 from '../assets/img/soldier11.jpg';
-import img8 from '../assets/img/wildrift.jpg'; 
-import img9 from '../assets/img/tof.jpg'; 
-import img10 from '../assets/img/robin.jpg'; 
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
 
 const games = [
   {
@@ -31,17 +18,10 @@ const games = [
     categories: ['MOBA', 'Populer'],
     rating: 4.5,
     items: [
-<<<<<<< HEAD
       { value: '11diamond', label: '11 Diamond', category: 'Currency', price: 100000 },
       { value: '209diamond', label: '209 Diamond', category: 'Currency', price: 300000 },
       { value: '256diamond', label: '256 Diamond', category: 'Currency', price: 400000 },
       { value: 'starlight', label: 'Starlight', category: 'Subscription', price: 500000 },
-=======
-      { value: '11diamond', label: '11 Diamond', price: 'Rp 100.000' },
-      { value: '209diamond', label: '209 Diamond', price: 'Rp 300.000' },
-      { value: '256diamond', label: '256 Diamond', price: 'Rp 400.000' },
-      { value: 'starlight', label: 'Starlight', price: 'Rp 500.000' },
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     ],
   },
   {
@@ -52,15 +32,9 @@ const games = [
     categories: ['RPG', 'Populer'],
     rating: 4.8,
     items: [
-<<<<<<< HEAD
       { value: '60primogems', label: '60 Primogems', category: 'Currency', price: 150000 },
       { value: '300crystals', label: '300 Crystals', category: 'Currency', price: 450000 },
       { value: 'welkin', label: 'Welkin Moon', category: 'Subscription', price: 600000 },
-=======
-      { value: '60primogems', label: '60 Primogems', price: 'Rp 150.000' },
-      { value: '300crystals', label: '300 Crystals', price: 'Rp 450.000' },
-      { value: 'welkin', label: 'Welkin Moon', price: 'Rp 600.000' },
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     ],
   },
   {
@@ -71,15 +45,9 @@ const games = [
     categories: ['Battle Royale', 'Populer'],
     rating: 4.3,
     items: [
-<<<<<<< HEAD
       { value: '60gold', label: '60 Gold', category: 'Currency', price: 50000 },
       { value: '300gold', label: '300 Gold', category: 'Currency', price: 200000 },
       { value: 'starlight', label: 'Starlight', category: 'Subscription', price: 400000 },
-=======
-      { value: '60gold', label: '60 Gold', price: 'Rp 50.000' },
-      { value: '300gold', label: '300 Gold', price: 'Rp 200.000' },
-      { value: 'starlight', label: 'Starlight', price: 'Rp 400.000' },
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     ],
   },
   {
@@ -90,15 +58,9 @@ const games = [
     categories: ['MOBA'],
     rating: 4.7,
     items: [
-<<<<<<< HEAD
       { value: '5skins', label: '5 Skins', category: 'Skin', price: 300000 },
       { value: '10skins', label: '10 Skins', category: 'Skin', price: 600000 },
       { value: '15skins', label: '15 Skins', category: 'Skin', price: 900000 },
-=======
-      { value: '5skins', label: '5 Skins', price: 'Rp 300.000' },
-      { value: '10skins', label: '10 Skins', price: 'Rp 600.000' },
-      { value: '15skins', label: '15 Skins', price: 'Rp 900.000' },
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     ],
   },
   {
@@ -109,13 +71,8 @@ const games = [
     categories: ['RPG'],
     rating: 4.6,
     items: [
-<<<<<<< HEAD
       { value: '200crystals', label: '200 Crystals', category: 'Currency', price: 250000 },
       { value: '500crystals', label: '500 Crystals', category: 'Currency', price: 450000 },
-=======
-      { value: '200crystals', label: '200 Crystals', price: 'Rp 250.000' },
-      { value: '500crystals', label: '500 Crystals', price: 'Rp 450.000' },
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     ],
   },
   {
@@ -126,15 +83,9 @@ const games = [
     categories: ['Battle Royale', 'Populer'],
     rating: 4.4,
     items: [
-<<<<<<< HEAD
       { value: '100uc', label: '100 UC', category: 'Currency', price: 120000 },
       { value: '500uc', label: '500 UC', category: 'Currency', price: 500000 },
       { value: '1000uc', label: '1000 UC', category: 'Currency', price: 900000 },
-=======
-      { value: '100uc', label: '100 UC', price: 'Rp 120.000' },
-      { value: '500uc', label: '500 UC', price: 'Rp 500.000' },
-      { value: '1000uc', label: '1000 UC', price: 'Rp 900.000' },
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     ],
   },
   {
@@ -145,13 +96,8 @@ const games = [
     categories: ['RPG'],
     rating: 4.5,
     items: [
-<<<<<<< HEAD
       { value: '30gems', label: '30 Gems', category: 'Currency', price: 75000 },
       { value: '100gems', label: '100 Gems', category: 'Currency', price: 200000 },
-=======
-      { value: '30gems', label: '30 Gems', price: 'Rp 75.000' },
-      { value: '100gems', label: '100 Gems', price: 'Rp 200.000' },
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     ],
   },
   {
@@ -162,13 +108,8 @@ const games = [
     categories: ['MOBA'],
     rating: 4.6,
     items: [
-<<<<<<< HEAD
       { value: '20skins', label: '20 Skins', category: 'Skin', price: 400000 },
       { value: '50skins', label: '50 Skins', category: 'Skin', price: 800000 },
-=======
-      { value: '20skins', label: '20 Skins', price: 'Rp 400.000' },
-      { value: '50skins', label: '50 Skins', price: 'Rp 800.000' },
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     ],
   },
   {
@@ -179,15 +120,9 @@ const games = [
     categories: ['RPG'],
     rating: 4.3,
     items: [
-<<<<<<< HEAD
       { value: '10gems', label: '10 Gems', category: 'Currency', price: 50000 },
       { value: '30gems', label: '30 Gems', category: 'Currency', price: 150000 },
       { value: '100gems', label: '100 Gems', category: 'Currency', price: 500000 },
-=======
-      { value: '10gems', label: '10 Gems', price: 'Rp 50.000' },
-      { value: '30gems', label: '30 Gems', price: 'Rp 150.000' },
-      { value: '100gems', label: '100 Gems', price: 'Rp 500.000' },
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     ],
   },
   {
@@ -198,15 +133,9 @@ const games = [
     categories: ['RPG'],
     rating: 4.3,
     items: [
-<<<<<<< HEAD
       { value: '10gems', label: '10 Gems', category: 'Currency', price: 50000 },
       { value: '30gems', label: '30 Gems', category: 'Currency', price: 150000 },
       { value: '100gems', label: '100 Gems', category: 'Currency', price: 500000 },
-=======
-      { value: '10gems', label: '10 Gems', price: 'Rp 50.000' },
-      { value: '30gems', label: '30 Gems', price: 'Rp 150.000' },
-      { value: '100gems', label: '100 Gems', price: 'Rp 500.000' },
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     ],
   },
 ];
--- src/pages/GameDetail.jsx
+++ src/pages/GameDetail.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-<<<<<<< HEAD
 import games from '../data/gamesData'; // Data game yang diimpor
 import { addTransaction } from '../data/invoice'; // Fungsi untuk menambah transaksi
 import { ClipboardIcon, CheckCircleIcon } from '@heroicons/react/24/outline'; // Ikon untuk notifikasi
@@ -23,35 +22,6 @@ const GameDetail = () => {
     }
     window.scrollTo(0, 0); // Menggulir ke atas saat komponen dimuat
 
-=======
-import games from '../data/gamesData';
-import { addTransaction } from '../data/invoice';
-import { ClipboardIcon, CheckCircleIcon } from '@heroicons/react/24/outline';
-
-const GameDetail = () => {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const [game, setGame] = useState(null);
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [whatsappNumber, setWhatsappNumber] = useState('');
-  const [accountId, setAccountId] = useState('');
-  const [error, setError] = useState({
-    accountId: '',
-    whatsappNumber: '',
-    selectedItem: '',
-  });
-  const [notification, setNotification] = useState({ visible: false, message: '', invoiceId: '' });
-  const [currentDate, setCurrentDate] = useState('');
-
-  useEffect(() => {
-    const foundGame = games.find((g) => g.id === parseInt(id));
-    if (foundGame) {
-      setGame(foundGame);
-    }
-    window.scrollTo(0, 0); // Scroll ke atas saat komponen dimuat
-
-    // Mengupdate waktu setiap detik
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     const interval = setInterval(() => {
       const now = new Date();
       const formattedTime = now.toLocaleString('id-ID', {
@@ -62,7 +32,6 @@ const GameDetail = () => {
         minute: '2-digit',
         second: '2-digit',
         hour12: false,
-<<<<<<< HEAD
         timeZone: 'Asia/Jakarta',
       }).replace(',', '').replace(/\//g, '-');
 
@@ -82,69 +51,24 @@ const GameDetail = () => {
     setError({ accountId: '', whatsappNumber: '', selectedItem: '' }); // Menghapus error sebelumnya
 
     // Validasi input
-=======
-        timeZone: 'Asia/Jakarta', // Ubah zona waktu sesuai kebutuhan
-      }).replace(',', '').replace(/\//g, '-'); // Mengganti / dengan -
-
-      setCurrentDate(formattedTime);
-    }, 1000);
-
-    return () => clearInterval(interval); // Bersihkan interval saat komponen tidak lagi digunakan
-  }, [id]);
-
-  const handleItemClick = (item) => {
-    setSelectedItem(item);
-    setError((prev) => ({ ...prev, selectedItem: '' }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Reset error messages
-    setError({
-      accountId: '',
-      whatsappNumber: '',
-      selectedItem: '',
-    });
-
-    // Validasi ID Game
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     if (!accountId) {
       setError((prev) => ({ ...prev, accountId: 'ID Game wajib diisi!' }));
       return;
     }
 
-<<<<<<< HEAD
-=======
-    // Validasi pilihan item
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     if (!selectedItem) {
       setError((prev) => ({ ...prev, selectedItem: 'Pilih salah satu item!' }));
       return;
     }
 
-<<<<<<< HEAD
-=======
-    // Validasi Nomor WhatsApp
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     if (!whatsappNumber) {
       setError((prev) => ({ ...prev, whatsappNumber: 'Nomor WhatsApp wajib diisi!' }));
       return;
     }
 
-<<<<<<< HEAD
     const invoiceNumber = `${game.id}${currentDate.replace(/[-: ]/g, '')}`; // Membuat nomor invoice
     const totalPrice = game.items.find(item => item.value === selectedItem).price; // Menemukan harga item
 
-=======
-    // Menghasilkan nomor invoice dengan waktu lokal
-    const invoiceNumber = `${game.id}${currentDate.replace(/[-: ]/g, '')}`; // Menghapus pemisah
-
-    // Menghitung total harga dari item yang dipilih
-    const totalPrice = game.items.find(item => item.value === selectedItem).price;
-
-    // Membuat data transaksi baru
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     const newTransaction = {
       id: (Date.now()).toString(),
       date: currentDate,
@@ -154,7 +78,6 @@ const GameDetail = () => {
       status: 'pending',
     };
 
-<<<<<<< HEAD
     addTransaction(newTransaction); // Menambahkan transaksi
 
     setNotification({ visible: true, message: 'Invoice berhasil dibuat!', invoiceId: invoiceNumber }); // Menampilkan notifikasi
@@ -162,18 +85,6 @@ const GameDetail = () => {
     setTimeout(() => {
       setNotification({ visible: false, message: '', invoiceId: '' });
       navigate('/cek-transaksi'); // Navigasi ke halaman cek transaksi setelah 3 detik
-=======
-    // Menambahkan transaksi ke Local Storage
-    addTransaction(newTransaction);
-
-    // Menampilkan notifikasi
-    setNotification({ visible: true, message: 'Invoice berhasil dibuat!', invoiceId: invoiceNumber });
-
-    // Mengalihkan pengguna ke halaman cek transaksi
-    setTimeout(() => {
-      setNotification({ visible: false, message: '', invoiceId: '' });
-      navigate('/cek-transaksi'); // Ganti '/cek-transaksi' dengan path yang sesuai
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
     }, 3000);
   };
 
@@ -191,19 +102,11 @@ const GameDetail = () => {
   };
 
   if (!game) {
-<<<<<<< HEAD
     return <div className="text-white">Loading...</div>; // Menampilkan loading jika data game belum ada
-=======
-    return <div className="text-white">Loading...</div>;
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
   }
 
   return (
     <div className="bg-gray-800 text-white p-6">
-<<<<<<< HEAD
-=======
-      {/* Notifikasi */}
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
       {notification.visible && (
         <div className="bg-green-600 text-white p-4 rounded-lg mb-4 text-center flex justify-between items-center shadow-lg transition-all duration-300">
           <div className="flex items-center">
@@ -211,11 +114,7 @@ const GameDetail = () => {
             <span className="font-bold">{notification.message}</span>
           </div>
           <button onClick={handleCopyInvoiceId} className="text-white ml-4 flex items-center hover:text-gray-300">
-<<<<<<< HEAD
             <ClipboardIcon className="h-5 w-5" aria-hidden="true" />
-=======
-            <ClipboardIcon className="h-5 w-5" aria-hidden="true" /> {/* Ikon salin */}
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
           </button>
         </div>
       )}
@@ -234,17 +133,10 @@ const GameDetail = () => {
           </div>
         </div>
 
-<<<<<<< HEAD
         <div className="md:w-1/2 mt-4 md:mt-0 bg-gray-900 p-4 rounded-lg">
           <form onSubmit={handleSubmit} className="mb-4">
             <div className="mb-2">
               <label htmlFor="accountId" className="block mb-1 text-gray-300">ID Game:</label>
-=======
-        <div className="md:w-1/2 mt-4 md:mt-0">
-          <form onSubmit={handleSubmit} className="mb-4">
-            <div className="mb-2">
-              <label htmlFor="accountId" className="block mb-1">ID Game:</label>
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
               <input
                 type="text"
                 id="accountId"
@@ -258,11 +150,7 @@ const GameDetail = () => {
               )}
             </div>
 
-<<<<<<< HEAD
             <h3 className="text-xl font-semibold text-white mb-2">Pilih Item:</h3>
-=======
-            <h3 className="text-xl font-semibold">Pilih Item:</h3>
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
             <div className="flex flex-wrap mb-4">
               {game.items.map((item) => (
                 <div
@@ -279,11 +167,7 @@ const GameDetail = () => {
             )}
 
             <div className="mb-2">
-<<<<<<< HEAD
               <label htmlFor="whatsapp" className="block mb-1 text-gray-300">Nomor WhatsApp:</label>
-=======
-              <label htmlFor="whatsapp" className="block mb-1">Nomor WhatsApp:</label>
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
               <input
                 type="text"
                 id="whatsapp"
@@ -298,15 +182,9 @@ const GameDetail = () => {
             </div>
             <button
               type="submit"
-<<<<<<< HEAD
               className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
             >
               Buat Invoice
-=======
-              className="bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
-            >
-              Kirim
->>>>>>> dd2b7074b9f133d2d7c8d6d8c82434a0d5fd1e16
             </button>
           </form>
         </div>
