7fdf323adc77d9bcdc7b16535860a08e8957cfaf
dwincahya <dwincahya8@gmail.com>
2024-10-14 19:00:35 +0700

    Light Store

--- src/pages/jualbeli.jsx
+++ src/pages/jualbeli.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Banner from '../components/Banner';
 
 function JualBeli() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []); 
+
   return (
     <div className="container mx-auto p-6">
       <Banner />
